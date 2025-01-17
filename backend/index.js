2320d3403d36946a04f4e0ac999b8e45517f3686
Jani Sarja <jani.sarja@live.com>
2025-01-17 13:02:41 +0200

    Add api endpoint for fetching poll data

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,6 @@ const app = express()
 const Poll = require('./models/Poll')
 const Option = require('./models/Option')
 
-const sequelize = new Sequelize(process.env.DATABASE_URL)
-
 const PORT = process.env.PORT || 3000
 
 app.use(cors())
@@ -17,22 +15,41 @@ app.use(express.json())
 app.use(express.static('dist'))
 
 app.post('/api/polls', async (req, res) => {
-  console.log('Poll created:', newPoll)
-  res.status(201).json(newPoll)
-
   try {
     const { title, options } = req.body
     const newPoll = await Poll.create({pollTitle: title, multipleOptions: true})
   
     options.map(async (option) => { 
-      console.log(option.optionText)
       await Option.create({
         pollId: newPoll.pollId,
         optionText: option
       }
     )})
+
+    return res.status(201).json(newPoll)
+
   } catch(error) {
     return res.status(400).json({ error })
+  }
+})
+
+app.get("/api/polls/:pollId", async (req, res) => {
+  try {
+    const pollId = req.params.pollId;
+
+    const poll = await Poll.findOne({
+      where: { pollId },
+      include: Option
+    })
+
+    if (!poll) {
+      return res.status(404).json({ message: "Poll not found" });
+    }
+
+    return res.json({ poll, options: poll.options });
+
+  } catch (error) {
+    return res.status(500).json({ error });
   }
 })
 
